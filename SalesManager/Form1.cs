db87198cb759df24a674b75b0b547b27a40d3783
Xiangyu Li <onlyyouleon@gamil.com>
2013-11-20 22:41:36 +0100

    Add test code for SQLite

--- SalesManager/Form1.cs
+++ SalesManager/Form1.cs
@@ -14,6 +14,30 @@ namespace SalesManager
         public Form1()
         {
             InitializeComponent();
+            Test();
+        }
+
+        private void Test()
+        {
+            try
+            {
+                System.Data.SQLite.SQLiteConnection conn = new System.Data.SQLite.SQLiteConnection();
+                System.Data.SQLite.SQLiteConnectionStringBuilder connstr = new System.Data.SQLite.SQLiteConnectionStringBuilder();
+                connstr.DataSource = "E:\\Projects\\SalesManager\\Database\\SalesDB";
+                //connstr.Password = "";//设置密码，SQLite ADO.NET实现了数据库密码保护
+                conn.ConnectionString = connstr.ToString();
+                conn.Open();
+                System.Data.SQLite.SQLiteCommand cmd = new System.Data.SQLite.SQLiteCommand();
+                string sql = "CREATE TABLE test(username varchar(20),password varchar(20))";
+                cmd.CommandText = sql;
+                cmd.Connection = conn;
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                return;
+            }
+            
         }
     }
 }
